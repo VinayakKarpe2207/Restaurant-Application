5b3019a9a978582fbcea2b78d40b3e6fbff10438
VinayakKarpe2207 <145591101+VinayakKarpe2207@users.noreply.github.com>
2025-01-29 12:36:32 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./app.css";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Success from "./Pages/Success";
